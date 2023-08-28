57b44cd6c759c49b15762f110c9eb465882284a4
Quinten <quinten.roets@gmail.com>
2023-08-28 18:08:13 +0200

    defer font-awesome loading

--- src/main.js
+++ src/main.js
@@ -3,19 +3,7 @@ import { createHead } from "unhead";
 import App from "./App.vue";
 import router from "./router";
 import VueGtag from "vue-gtag";
-
-// icons
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
-import {
-  faMapMarkerAlt,
-  faEnvelope,
-  faGlobe,
-} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-library.add(faLinkedin, faGithub, faGlobe, faMapMarkerAlt, faEnvelope); // add the location symbol to the library
-
 import "@/assets/css/tailwind.css";
 
 const head = createHead();
