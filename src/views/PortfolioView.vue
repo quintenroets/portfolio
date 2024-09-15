f6ca51eaa07ab6ffc70478d74d2e342beaa1d5a9
Quinten <quinten.roets@gmail.com>
2024-09-15 09:52:21 -0700

    add github statistics

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
     <h1>Projects</h1>
+    <img
+      src="https://github-readme-stats.vercel.app/api/?username=quintenroets&theme=transparent&text_color=2c3e50&title_color=222222&hide_border=true&locale=en&show_icons=true&hide_rank=true&custom_title=Github Statistics&disable_animations=true&hide=issues"
+      alt="GitHub Stats"
+    />
+    <img
+      src="https://github-readme-stats.vercel.app/api/top-langs?username=quintenroets&layout=compact&theme=transparent&text_color=2c3e50&title_color=222222&hide_border=true&locale=en&langs_count=20"
+      alt="GitHub Stats"
+    />
+    <img
+      src="https://github-readme-streak-stats.herokuapp.com/?user=quintenroets&theme=transparent&text_color=2c3e50&title_color=222222&hide_current_streak=true&hide_longest_streak=true&card_width=200&card_height=50&hide_border=true&dates=222222&sideLabels=222222&sideNums=222222"
+      alt="GitHub Stats"
+    />
+    <img
+      src="https://github-readme-stats.vercel.app/api/pin/?username=quintenroets&repo=package-dev-tools"
+      alt=""
+      href="https://github-readme-stats.vercel.app/api/pin/?username=quintenroets&repo=package-dev-tools"
+    />
     <div class="flex justify-center">
       <div class="max-w-2xl text-left mt-4">
         <p>
