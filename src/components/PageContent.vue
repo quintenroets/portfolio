773fb232f44103e13af06efadf872b19813735e3
Quinten Roets <62651391+quintenroets@users.noreply.github.com>
2024-10-06 20:01:24 -0700

    fix descriptions (#5)

--- src/components/PageContent.vue
+++ src/components/PageContent.vue
@@ -2,7 +2,7 @@
   <div
     class="flex-grow mx-1 my-5 overflow-y-auto"
     :class="{
-      'mb-20 bg-orange-200': ratio < 1,
+      'mb-20': ratio < 1,
     }"
   >
     <router-view class="px-4 overflow-y-auto h-full" />
